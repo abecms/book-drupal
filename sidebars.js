0f756504dd066cfefc1e15f83101cf3b50f6dd0b
Greg Besson <bessong@gmail.com>
2020-02-17 22:48:08 +0100

    doc

--- sidebars.js
+++ sidebars.js
@@ -9,7 +9,7 @@ module.exports = {
   docs: {
     Setup: ['introduction', 'setup'],
     Configuration: ['configuration', 'multilingue', 'tests', 'webforms'],
-    Dynamisation: ['nodes', 'paragraphs', 'views', 'forms', 'taxonomies', 'tokens', 'images', 'users', 'comments', 'newsletters', 'menus', 'ckeditor', 'modules'],
+    Dynamisation: ['nodes', 'paragraphs', 'views', 'forms', 'taxonomies', 'tokens', 'images', 'users', 'comments', 'newsletters', 'menus', 'ckeditor', 'modules', 'coding'],
     'Dev specific': ['dev'],
   },
 };
